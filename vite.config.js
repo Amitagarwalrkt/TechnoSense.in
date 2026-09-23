a9b2a91339e6d99a784b845ebfc465c87cba9344
Amit <amitagarwalrkt@gmail.com>
2026-09-23 13:40:17 +0530

    update json

--- vite.config.js
+++ vite.config.js
@@ -107,7 +107,6 @@ export default defineConfig({
   },
   server: {
     host: 'localhost',
-<<<<<<< HEAD
     port: 5173,
     proxy: {
       '/api': {
@@ -116,8 +115,6 @@ export default defineConfig({
       },
     },
   },
-=======
     port: 5173
   }
->>>>>>> d7f8d5c373b003c3772ecc9af0874266fc016599
 })
